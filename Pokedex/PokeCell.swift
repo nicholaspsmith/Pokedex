f3529c439e76fbb87bd5787f30df5c592b134af4
Nicholas Smith <npsmith1990@gmail.com>
2016-01-23 22:55:05 -0600

    Create custom cell

---
+++ Pokedex/PokeCell.swift
@@ -0,0 +1,24 @@
+//
+//  PokeCell.swift
+//  Pokedex
+//
+//  Created by Nick on 1/23/16.
+//  Copyright © 2016 Nick. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    func configureCell(pokemon: Pokemon) {
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalizedString
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+    
+}
