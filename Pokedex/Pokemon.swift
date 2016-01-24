0c47f2010b3a423d9f230041e3cf16a28215a9d2
Nicholas Smith <npsmith1990@gmail.com>
2016-01-23 22:32:09 -0600

    Create pokemon class

---
+++ Pokedex/Pokemon.swift
@@ -0,0 +1,27 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by Nick on 1/23/16.
+//  Copyright © 2016 Nick. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        _name = name
+        _pokedexId = pokedexId
+    }
+}
