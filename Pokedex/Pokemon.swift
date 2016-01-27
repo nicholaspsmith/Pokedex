127d1db1c51f9fb8edb9b817089679a03852a518
Nicholas Smith <npsmith1990@gmail.com>
2016-01-27 16:29:18 -0600

    Set properties of pokemon by calling poke api

--- Pokedex/Pokemon.swift
+++ Pokedex/Pokemon.swift
@@ -18,8 +18,9 @@ class Pokemon {
     private var _height: String!
     private var _weight: String!
     private var _baseattack: String!
-    private var _nextEvolution: String!
-    
+    private var _nextEvolutionTxt: String!
+    private var _nextEvolutionId: String!
+    private var _nextEvolutionLvl: String!
     private var _pokemonUrl: String!
     
     // Calls pokemon api
@@ -46,7 +47,90 @@ class Pokemon {
         
         Alamofire.request(.GET, url).responseJSON { response in
             let result = response.result
-            print(result.debugDescription)
+            
+            if let dict = result.value as? Dictionary<String, AnyObject> {
+                
+                if let weight = dict["weight"] as? String {
+                    self._weight = weight
+                }
+                
+                if let height = dict["height"] as? String {
+                    self._height = height
+                }
+                
+                if let attack = dict["attack"] as? Int {
+                    self._baseattack = "\(attack)"
+                }
+                
+                if let defense = dict["defense"] as? Int {
+                    self._defense = "\(defense)"
+                }
+                
+                if let types = dict["types"] as? [Dictionary<String, String>] where types.count > 0 {
+                    if let type = types[0]["name"] {
+                        self._type = type.capitalizedString
+                    }
+                    
+                    if types.count > 1 {
+                        for var x = 1; x < types.count; x++ {
+                            if let name = types[x]["name"] {
+                                self._type! += "/\(name.capitalizedString)"
+                            }
+                        }
+                    }
+                } else {
+                    self._type = ""
+                }
+                
+                if let descArr = dict["descriptions"] as? [Dictionary<String, String>] where descArr.count > 0 {
+                    
+                    if let url = descArr[0]["resource_uri"] {
+                        let nsurl = NSURL(string: "\(URL_BASE)\(url)")!
+                        
+                        Alamofire.request(.GET, nsurl).responseJSON { response in
+                            
+                            let desResult = response.result
+                            if let descDict = desResult.value as? Dictionary<String, AnyObject> {
+                                
+                                if let description = descDict["description"] as? String {
+                                    self._description = description
+                                    print(self._description)
+                                }
+                            }
+                            completed()
+                        }
+                    }
+                    
+                } else {
+                    self._description = ""
+                }
+                
+                if let evolutions = dict["evolutions"] as? [Dictionary<String, AnyObject>] where evolutions.count > 0 {
+                    
+                    if let to = evolutions[0]["to"] as? String {
+                        
+                        if to.rangeOfString("mega") == nil {
+                            // Ignore "mega" evolutions
+                            if let uri = evolutions[0]["resource_uri"] as? String {
+                                let newStr = uri.stringByReplacingOccurrencesOfString(URL_POKEMON, withString: "")
+                                
+                                let num = newStr.stringByReplacingOccurrencesOfString("/", withString: "")
+                                
+                                self._nextEvolutionId = num
+                                self._nextEvolutionTxt = to
+                                
+                                if let lvl = evolutions[0]["level"] as? Int {
+                                    self._nextEvolutionLvl = "\(lvl)"
+                                }
+                                
+                                print(self._nextEvolutionLvl)
+                            }
+                        }
+                    }
+                }
+                
+                
+            }
         }
         
     }
