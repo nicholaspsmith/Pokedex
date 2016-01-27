7d712d770f53460e8fba4f2435643e6b74506055
Nicholas Smith <npsmith1990@gmail.com>
2016-01-26 21:10:27 -0600

    Revert "Revert "Fix broken project; allow click on table cell to open new view""
    This reverts commit e517054dca0a30e082d7a8ccb6b34568b27bb654.

--- Pokedex/ViewController.swift
+++ Pokedex/ViewController.swift
@@ -89,6 +89,15 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     
     func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
         
+        let poke: Pokemon!
+        
+        if inSearchMode {
+            poke = filteredPokemon[indexPath.row]
+        } else {
+            poke = pokemon[indexPath.row]
+        }
+        
+        performSegueWithIdentifier("PokeDetailVC", sender: poke)
     }
     
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -134,5 +143,14 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         }
     }
 
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "PokeDetailVC" {
+            if let detailsVC = segue.destinationViewController as? PokeDetailVC {
+                if let poke = sender as? Pokemon {
+                     detailsVC.pokemon = poke
+                }
+            }
+        }
+    }
 }
 
